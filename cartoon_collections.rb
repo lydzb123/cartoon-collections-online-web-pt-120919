2a86b9a0912a22e589c67b2372d3fce2bbf7ec1e
Lydia B <lydiabaik@gmail.com>
2019-12-18 08:25:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,6 @@ calls.any? {|w| w.length > 4}
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_types)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
