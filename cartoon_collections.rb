434098e28561ae4b89e1c2c7cff1e2bdaa3cef8c
Lydia B <lydiabaik@gmail.com>
2019-12-18 07:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  roll_call = []
+  dwarves.each.with_index(1){|dwarf, index| roll_call << "#{index}.*#{dwarf}"}
+  print roll_call
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(veggies)
+  veggies.map{|w| "#{w.upcase}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
+#
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
