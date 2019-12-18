a622bb99db0e56c2d3cb7097f27fc707fa7883ae
Lydia B <lydiabaik@gmail.com>
2019-12-18 08:04:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,14 @@ end
 
 
 def long_planeteer_calls(calls)
-  i = 0
+  count = 0
   if i.length > 4
     true
   else
     false
-    i += 1
+    count += 1
   end
-  
+
 end
 
 # def find_the_cheese# code an argument here
